e22219386583f2b9340cb72a29d8d4af94075b36
zparmenter <zparmenter20@gmail.com>
2024-01-07 18:45:24 -0600

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(cors());
 
